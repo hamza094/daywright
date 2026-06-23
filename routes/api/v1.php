a9cf34b5da6be0ef1b5e30ff767c1ebaaf882f65
Hamza <hamza_pisces@live.com>
2026-06-23 16:19:33 +0500

    fix(conversation): apply subscription and idempotent middleware to store conversation
    add related tests

--- routes/api/v1.php
+++ routes/api/v1.php
@@ -147,7 +147,8 @@ Route::middleware(['auth:sanctum'])->group(function (): void {
 
                 // Chat Conversation Routes
                 Route::apiResource('/conversations', ConversationController::class)
-                    ->only(['store', 'destroy', 'index']);
+                    ->only(['store', 'destroy', 'index'])
+                    ->middleware(['subscription', Idempotent::using(scope: IdempotencyScope::User)], ['only' => ['store']]);
             });
 
             Route::middleware(['can:access,project'])->group(function (): void {
