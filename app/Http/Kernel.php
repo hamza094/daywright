6ceff7de17cd07e7f077d2ecf8bec6435ec3aa89
Hamza <hamza_pisces@live.com>
2026-07-27 11:52:03 +0500

    feat(logs-middleware): implement request id via middleware in each logs response

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -19,6 +19,7 @@ class Kernel extends HttpKernel
     protected $middleware = [
         \Illuminate\Foundation\Http\Middleware\InvokeDeferredCallbacks::class,
         Middleware\TrustProxies::class,
+        Middleware\AttachRequestId::class,
         \Illuminate\Http\Middleware\HandleCors::class,
         Middleware\CheckForMaintenanceMode::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
